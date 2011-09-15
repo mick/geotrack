33484a86938d218d21ce9fb0455c3eb56fe3dc3d
Mick Thompson <dthompson@gmail.com>
2011-09-15 09:55:19 -0600

    fix spatial view name

--- server/app/server.js
+++ server/app/server.js
@@ -24,7 +24,7 @@ socket.on('connection', function(client){
         console.log("get initial: ", msg);
         if(msg.type == "fetchinitial"){
             bbox = msg.bbox.join(",");
-            db.spatial("gc-utils/geomFull",
+            db.spatial("gc-utils/geomsFull",
                        {"bbox":bbox, "descending": "true"},
                        function(er, docs) {
                            if(er){
